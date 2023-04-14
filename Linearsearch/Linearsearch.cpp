a78a391ee7563ce09f958cdb65ba270c9a0b8c15
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-04-14 18:21:38 +0700

    prosedur input menggunakan while

--- Linearsearch/Linearsearch.cpp
+++ Linearsearch/Linearsearch.cpp
@@ -5,3 +5,16 @@ int arr[20]; // Array to be search
 int n; //Number of elements in the array
 int i; //index of array element
 
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the array:";
+        cin >> n;
+        if ((n < 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
+
+    
